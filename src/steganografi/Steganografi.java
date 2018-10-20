68aad22770dbf82079bb686690ce04a6ed8b4362
ivenacindy <37652346+ivenacindy@users.noreply.github.com>
2018-10-21 00:48:08 +0700

    Steganografi

--- src/steganografi/Steganografi.java
+++ src/steganografi/Steganografi.java
@@ -11,7 +11,6 @@ package steganografi;
  */
 
 import java.io.File;
-import java.awt.Point;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
@@ -21,10 +20,6 @@ import javax.swing.JOptionPane;
 
 public class Steganografi {
 
-    /**
-     * @param args the command line arguments
-     */
-    
     public Steganografi(){
     }
     
@@ -41,7 +36,6 @@ public class Steganografi {
     public String decode(String path, String name){
 	byte[] decode;
 	try{
-            //user space is necessary for decrypting
             BufferedImage image  = user_space(getImage(image_path(path,name,"png")));
             decode = decode_text(get_byte_data(image));
             return(new String(decode));
@@ -58,20 +52,20 @@ public class Steganografi {
     }
 
     private BufferedImage getImage(String f){
-	BufferedImage 	image	= null;
-	File 		file 	= new File(f);		
+	BufferedImage image = null;
+	File file = new File(f);		
 	try{
             image = ImageIO.read(file);
         }catch(Exception ex){
             JOptionPane.showMessageDialog(null, 
-            "Image could not be read!","Error",JOptionPane.ERROR_MESSAGE);
+                "Image could not be read!","Error",JOptionPane.ERROR_MESSAGE);
 	}
 	return image;
     }
 	
     private boolean setImage(BufferedImage image, File file, String ext){
 	try{
-            file.delete(); //delete resources used by the File
+            file.delete(); 
             ImageIO.write(image,ext,file);
             return true;
 	}catch(Exception e){
@@ -86,8 +80,8 @@ public class Steganografi {
 	byte msg[] = text.getBytes();
 	byte len[]   = bit_conversion(msg.length);
 	try{
-            encode_text(img, len,  0); //0 first positiong
-            encode_text(img, msg, 32); //4 bytes of space for length: 4bytes*8bit = 32 bits
+            encode_text(img, len,  0); 
+            encode_text(img, msg, 32); 
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, 
                 "Target File cannot hold message!", "Error",JOptionPane.ERROR_MESSAGE);
@@ -96,93 +90,57 @@ public class Steganografi {
     }
 	
     private BufferedImage user_space(BufferedImage image){
-        BufferedImage new_img  = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-	Graphics2D	graphics = new_img.createGraphics();
+        BufferedImage new_img = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+	Graphics2D graphics = new_img.createGraphics();
         graphics.drawRenderedImage(image, null);
-	graphics.dispose(); //release all allocated memory for this image
+	graphics.dispose(); 
         return new_img;
     }
 
     private byte[] get_byte_data(BufferedImage image){
-	WritableRaster raster   = image.getRaster();
+	WritableRaster raster = image.getRaster();
 	DataBufferByte buffer = (DataBufferByte)raster.getDataBuffer();
         return buffer.getData();
     }
 
     private byte[] bit_conversion(int i){
-        //originally integers (ints) cast into bytes
-	//byte byte7 = (byte)((i & 0xFF00000000000000L) >>> 56);
-	//byte byte6 = (byte)((i & 0x00FF000000000000L) >>> 48);
-	//byte byte5 = (byte)((i & 0x0000FF0000000000L) >>> 40);
-	//byte byte4 = (byte)((i & 0x000000FF00000000L) >>> 32);
-		
-	//only using 4 bytes
-	byte byte3 = (byte)((i & 0xFF000000) >>> 24); //0
-	byte byte2 = (byte)((i & 0x00FF0000) >>> 16); //0
-	byte byte1 = (byte)((i & 0x0000FF00) >>> 8 ); //0
+	byte byte3 = (byte)((i & 0xFF000000) >>> 24);
+	byte byte2 = (byte)((i & 0x00FF0000) >>> 16);
+	byte byte1 = (byte)((i & 0x0000FF00) >>> 8 );
 	byte byte0 = (byte)((i & 0x000000FF)	   );
-	//{0,0,0,byte0} is equivalent, since all shifts >=8 will be 0
+        
 	return(new byte[]{byte3,byte2,byte1,byte0});
     }
 
-    private byte[] encode_text(byte[] image, byte[] addition, int offset)
-	{
-		//check that the data + offset will fit in the image
-		if(addition.length + offset > image.length)
-		{
-			throw new IllegalArgumentException("File not long enough!");
-		}
-		//loop through each addition byte
-		for(int i=0; i<addition.length; ++i)
-		{
-			//loop through the 8 bits of each byte
-			int add = addition[i];
-			for(int bit=7; bit>=0; --bit, ++offset) //ensure the new offset value carries on through both loops
-			{
-				//assign an integer to b, shifted by bit spaces AND 1
-				//a single bit of the current byte
-				int b = (add >>> bit) & 1;
-				//assign the bit by taking: [(previous byte value) AND 0xfe] OR bit to add
-				//changes the last bit of the byte in the image to be the bit of addition
-				image[offset] = (byte)((image[offset] & 0xFE) | b );
-			}
-		}
-		return image;
+    private byte[] encode_text(byte[] image, byte[] addition, int offset){
+	if(addition.length + offset > image.length){
+            throw new IllegalArgumentException("File not long enough!");
 	}
-	
-	/*
-	 *Retrieves hidden text from an image
-	 *@param image Array of data, representing an image
-	 *@return Array of data which contains the hidden text
-	 */
-	private byte[] decode_text(byte[] image)
-	{
-		int length = 0;
-		int offset  = 32;
-		//loop through 32 bytes of data to determine text length
-		for(int i=0; i<32; ++i) //i=24 will also work, as only the 4th byte contains real data
-		{
-			length = (length << 1) | (image[i] & 1);
-		}
-		
-		byte[] result = new byte[length];
-		
-		//loop through each byte of text
-		for(int b=0; b<result.length; ++b )
-		{
-			//loop through each bit within a byte of text
-			for(int i=0; i<8; ++i, ++offset)
-			{
-				//assign bit: [(new byte value) << 1] OR [(text byte) AND 1]
-				result[b] = (byte)((result[b] << 1) | (image[offset] & 1));
-			}
-		}
-		return result;
-	}
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
         
+        for(int i=0; i<addition.length; ++i){
+            int add = addition[i];
+            for(int bit=7; bit>=0; --bit, ++offset){
+                int b = (add >>> bit) & 1;
+		image[offset] = (byte)((image[offset] & 0xFE) | b );
+            }
+	}
+        return image;
     }
     
+    private byte[] decode_text(byte[] image){
+        int length = 0;
+        int offset  = 32;
+        for(int i=0; i<32; ++i){
+            length = (length << 1) | (image[i] & 1);
+        }
+
+        byte[] result = new byte[length];
+
+        for(int b=0; b<result.length; ++b ){
+            for(int i=0; i<8; ++i, ++offset){
+                result[b] = (byte)((result[b] << 1) | (image[offset] & 1));
+            }
+        }
+        return result;
+    }
 }
---
+++ src/steganografi/Vigenere.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package steganografi;
+
+/**
+ *
+ * @author ivenacindy
+ */
+import java.nio.charset.StandardCharsets;
+
+public class Vigenere {
+    int[] aKey = null;
+    int[] aPlain = null;
+    int[] hasilEnkrip = null;
+    // Extended ASCII Converter
+    public static final char[] EXTENDED = { 0x007f,0x0080,0x0081,0x0082,
+            0x0083,0x0084,0x0085,0x0086,0x0087,0x0088,0x0089,
+            0x008a,0x008b,0x008c,0x008d,0x008e,0x008f,0x0090,
+            0x0091,0x0092,0x0093,0x0094,0x0095,0x0096,0x0097,0x0098,0x0099,
+            0x009a,0x009b,0x009c,0x009d,0x009e,0x009f,
+            0x00a1,0x00a2,0x00a3,0x00a4,0x00a5,0x00a6,0x00a7,0x00a8,0x00a9,
+            0x00aa,0x00ab,0x00ac,0x00ad,0x00ae,0x00af,0x00b0,
+            0x00b1,0x00b2,0x00b3,0x00b4,0x00b5,0x00b6,0x00b7,0x00b8,0x00b9,
+            0x00ba,0x00bb,0x00bc,0x00bd,0x00be,0x00bf,0x00c0,
+            0x00c1,0x00c2,0x00c3,0x00c4,0x00c5,0x00c6,0x00c7,0x00c8,0x00c9,
+            0x00ca,0x00cb,0x00cc,0x00cd,0x00ce,0x00cf,0x00d0,
+            0x00d1,0x00d2,0x00d3,0x00d4,0x00d5,0x00d6,0x00d7,0x00d8,0x00d9,
+            0x00da,0x00db,0x00dc,0x00dd,0x00de,0x00df,0x00e0,
+            0x00e1,0x00e2,0x00e3,0x00e4,0x00e5,0x00e6,0x00e7,0x00e8,0x00e9,
+            0x00ea,0x00eb,0x00ec,0x00ed,0x00ee,0x00ef,0x00f0};
+
+    public static final char getAscii(int code) {
+        if (code >= 0x80 && code <= 0xFF) {
+            return EXTENDED[code - 128];
+        }
+        return (char) code;
+    }
+    
+    public int CekExtended(char a){
+        for (int i = 0; i < EXTENDED.length; i++) {
+            if(a == EXTENDED[i]){
+                return i+128;
+            }
+        }
+        return 63;
+    }
+    
+    public static final char printChar(int code) {
+        return getAscii(code);
+    }
+    
+    public void plainToAscii(String kata){
+        byte[] aa = kata.getBytes(StandardCharsets.US_ASCII);
+        aPlain = new int [aa.length];
+        for (int i = 0; i < aa.length; i++) {
+            if(aa[i] == 63){
+                aPlain[i]=CekExtended(kata.charAt(i));
+            }else{
+                aPlain[i] = aa[i];
+            }
+//            System.out.println("asci plain : " + aPlain[i]);
+        }
+    }
+    
+    public void keyToAscii(String kata){
+        byte[] aa = kata.getBytes(StandardCharsets.US_ASCII);
+        aKey = new int[aPlain.length];
+        int count = 0;
+            for (int i = 0; i < aPlain.length; i++) {
+                if (count >= aa.length){
+                    count = 0;
+                }
+                aKey[i] = aa[count];
+//                System.out.println("asci key : " + aKey[i]);
+                count++;
+            }
+    }
+    
+    public String Enkrip (String plain, String key){        
+        plainToAscii(plain);
+        keyToAscii(key);
+        String enkrip;
+        char[] x = new char[aPlain.length];
+        for (int i = 0; i < aPlain.length; i++) {
+//            System.out.println("hasil : " + (i+1)+" " + (aKey[i] + aPlain[i])%256);
+            x[i] = printChar((aKey[i] + aPlain[i])%256); 
+        }
+        enkrip = String.valueOf(x);
+        System.out.println(enkrip);
+        System.out.println(Dekrip(enkrip, key));
+        return enkrip;
+    }
+    
+    public String Dekrip (String plain, String key){
+        String dekrip;
+        System.out.println(plain);
+        plainToAscii(plain);
+        keyToAscii(key);
+        char[] x = new char[aPlain.length];
+        for (int i = 0; i < aPlain.length; i++) {
+//            System.out.println(aPlain[i]);
+            x[i] = printChar((aPlain[i]-aKey[i] )%256); 
+        }
+        dekrip = String.valueOf(x);
+//        System.out.println(enkrip);
+        return dekrip;
+    }
+}
